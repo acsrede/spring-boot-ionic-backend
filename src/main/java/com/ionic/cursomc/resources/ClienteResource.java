356b419d0d970192f1ca2b6afb402079c7322fe2
acsrede <acs.rede@gmail.com>
2018-03-13 21:36:54 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/ionic/cursomc/resources/ClienteResource.java
@@ -0,0 +1,28 @@
+package com.ionic.cursomc.resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ionic.cursomc.domain.Cliente;
+import com.ionic.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)	
+	public ResponseEntity<?> findId(@PathVariable Integer id) {		
+		Optional<Cliente> obj = clienteService.buscaPorId(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/ionic/cursomc/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.ionic.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ionic.cursomc.domain.Cliente;
+import com.ionic.cursomc.repositories.ClienteRepository;
+import com.ionic.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public Optional<Cliente> buscaPorId(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		// is null
+		if (!obj.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + 
+					id + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+	
+}
