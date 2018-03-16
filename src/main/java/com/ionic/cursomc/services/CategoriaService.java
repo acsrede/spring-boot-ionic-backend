884aab3f2fe49541e8ae3da659912c4e9bf1ddfb
acsrede <acs.rede@gmail.com>
2018-03-16 15:32:01 -0300

    Paginacao com parametro opcional na requisicao

--- src/main/java/com/ionic/cursomc/services/CategoriaService.java
+++ src/main/java/com/ionic/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ionic.cursomc.domain.Categoria;
@@ -41,6 +44,7 @@ public class CategoriaService {
 		return categoriaRepository.save(obj);
 	}
 
+	//delete category
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -50,7 +54,13 @@ public class CategoriaService {
 		}				
 	}
 
+	//list categories
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 }
