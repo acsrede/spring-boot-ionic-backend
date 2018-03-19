ec45cb08757c2c500e69ba9f8298ef7387c78a57
acsrede <acs.rede@gmail.com>
2018-03-19 09:54:39 -0300

    Validacao customizada: evita email repetido no insert cliente

--- src/main/java/com/ionic/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/ionic/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ionic.cursomc.domain.Cliente;
 import com.ionic.cursomc.domain.enums.TipoCliente;
 import com.ionic.cursomc.dto.ClienteNewDTO;
+import com.ionic.cursomc.repositories.ClienteRepository;
 import com.ionic.cursomc.resources.exception.FieldMessage;
 import com.ionic.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {}
@@ -27,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido!"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente!")); 
 		}
 		
 		for (FieldMessage e : list) {
