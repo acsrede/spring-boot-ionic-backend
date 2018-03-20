c701d27362ec8ceb51cd8b870c91431b54d059aa
acsrede <acs.rede@gmail.com>
2018-03-20 14:21:29 -0300

    Inserindo Pedido

--- src/main/java/com/ionic/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/ionic/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.ionic.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.ionic.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/ionic/cursomc/services/PedidoService.java
+++ src/main/java/com/ionic/cursomc/services/PedidoService.java
@@ -1,13 +1,20 @@
 package com.ionic.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ionic.cursomc.domain.Categoria;
+import com.ionic.cursomc.domain.ItemPedido;
+import com.ionic.cursomc.domain.PagamentoComBoleto;
 import com.ionic.cursomc.domain.Pedido;
+import com.ionic.cursomc.domain.enums.EstadoPagamento;
+import com.ionic.cursomc.repositories.ItemPedidoRepository;
+import com.ionic.cursomc.repositories.PagamentoRepository;
 import com.ionic.cursomc.repositories.PedidoRepository;
+import com.ionic.cursomc.repositories.ProdutoRepository;
 import com.ionic.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,11 +22,43 @@ public class PedidoService {
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = pedidoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = pedidoRepository.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoRepository.findById(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 
 }
