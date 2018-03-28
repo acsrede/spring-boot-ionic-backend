0ba60910e1b314cda64a217fb8a29cc19f3841e9
acsrede <acs.rede@gmail.com>
2018-03-28 16:24:42 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/ionic/cursomc/config/SecurityConfig.java
+++ src/main/java/com/ionic/cursomc/config/SecurityConfig.java
@@ -7,26 +7,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.ionic.cursomc.security.JWTAuthenticationFilter;
+import com.ionic.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
 	private Environment environment;
+
+	@Autowired
+	private JWTUtil jwtUtil;
 
 	public static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
-	public static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**" };
-	
+	public static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**", "/estados/**" };
+
+	private static final String[] PUBLIC_MATCHERS_POST = { "/clientes/**", "/auth/forgot/**" };
+
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 
@@ -34,12 +47,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			httpSecurity.headers().frameOptions().disable();
 		}
 
-		httpSecurity.cors().and().csrf().disable()
-			.authorizeRequests()
+		httpSecurity.cors().and().csrf().disable().authorizeRequests()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-			.antMatchers(PUBLIC_MATCHERS).permitAll()
-			.anyRequest().authenticated().and()
+			.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated().and()
+			.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil))
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
 	@Bean
@@ -48,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
