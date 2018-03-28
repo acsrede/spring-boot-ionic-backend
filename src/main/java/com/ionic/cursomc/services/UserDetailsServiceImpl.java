0439496f461a8ba40c0d9dc9320287001820664a
acsrede <acs.rede@gmail.com>
2018-03-27 21:28:14 -0300

    code security incomplete

---
+++ src/main/java/com/ionic/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ionic.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.ionic.cursomc.domain.Cliente;
+import com.ionic.cursomc.repositories.ClienteRepository;
+import com.ionic.cursomc.security.UserSS;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = clienteRepository.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
