d9f77636036a9c211d9820533dc960a7429c3e3b
acsrede <acs.rede@gmail.com>
2018-03-30 22:15:46 -0300

    Refresh token

--- src/main/java/com/ionic/cursomc/services/UserService.java
+++ src/main/java/com/ionic/cursomc/services/UserService.java
@@ -6,7 +6,7 @@ import com.ionic.cursomc.security.UserSS;
 
 public class UserService {
 
-	public static UserSS authenicated() {
+	public static UserSS authenticated() {
 		try {
 			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		} catch (Exception e) {
